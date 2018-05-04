1f60c245263b4e8a95dda8306747d79539f576fd
Fernando Souza Silva <fernandos@spi.local>
2018-05-04 10:56:08 -0300

    update configuration

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace qualityservice
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls ("http://*:5014")
                 .UseStartup<Startup>()
                 .Build();
     }
