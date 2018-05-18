65adf48297de4b11ca683ca07b55bac81b2e2406
fernandat <fefedel9@hotmail.com>
2018-05-18 14:09:40 -0300

    security implemented

--- Controllers/AnalysisController.cs
+++ Controllers/AnalysisController.cs
@@ -1,39 +1,33 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using qualityservice.Service.Interface;
 using qualityservice.Model;
+using qualityservice.Service.Interface;
+using securityfilter;
 
-namespace qualityservice.Controllers
-{
-    [Route("api/ProductionOrderQuality/[controller]")]
-    public class AnalysisController : Controller
-    {
+namespace qualityservice.Controllers {
+    [Route ("api/ProductionOrderQuality/[controller]")]
+    public class AnalysisController : Controller {
         private readonly IAnalysisService _analysisService;
 
-        public AnalysisController(IAnalysisService analysisService)
-        {
+        public AnalysisController (IAnalysisService analysisService) {
             _analysisService = analysisService;
         }
 
+        [HttpPost ("productionOrder/{productionOrderId}")]
+        [SecurityFilter ("quality__allow_update")]
+        public async Task<IActionResult> PostProductionOrder (int productionOrderId, [FromBody] Analysis analysis) {
+            try {
+                if (ModelState.IsValid) {
+                    var analysisDb = await _analysisService.AddAnalysis (productionOrderId, analysis);
 
-        [HttpPost("productionOrder/{productionOrderId}")]
-        public async Task<IActionResult> PostProductionOrder(int productionOrderId,[FromBody] Analysis analysis)
-        {
-            try{
-                if (ModelState.IsValid)
-                {
-                    var analysisDb = await _analysisService.AddAnalysis(productionOrderId,analysis);
-
-                     return Created($"api/productionOrderQuality/{analysisDb.analysisId}", analysisDb);
-                }   
-            return BadRequest(ModelState);
-            }
-            catch(Exception ex)
-            {
-                return StatusCode(500, ex.Message);
+                    return Created ($"api/productionOrderQuality/{analysisDb.analysisId}", analysisDb);
+                }
+                return BadRequest (ModelState);
+            } catch (Exception ex) {
+                return StatusCode (500, ex.Message);
             }
         }
-        
+
     }
 }
--- Controllers/ProductionOrderQualityController.cs
+++ Controllers/ProductionOrderQualityController.cs
@@ -1,177 +1,154 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using qualityservice.Service.Interface;
 using qualityservice.Model.ProductionOrderApi;
+using qualityservice.Service.Interface;
+using securityfilter;
 
-namespace qualityservice.Controllers
-{
-    [Route("api/[controller]")]
-    public class ProductionOrderQualityController : Controller
-    {
+namespace qualityservice.Controllers {
+    [Route ("api/[controller]")]
+    public class ProductionOrderQualityController : Controller {
         private readonly IProductionOrderQualityService _productionOrderQualityService;
 
-        public ProductionOrderQualityController(IProductionOrderQualityService productionOrderQualityService)
-        {
+        public ProductionOrderQualityController (IProductionOrderQualityService productionOrderQualityService) {
             _productionOrderQualityService = productionOrderQualityService;
         }
 
-        [HttpGet("status/{status}")]
-        public async Task<IActionResult> Get(string status,[FromQuery]int startat,[FromQuery]int quantity)
-        {
-            try{
-                if(quantity == 0)
-                    quantity =50;    
+        [HttpGet ("status/{status}")]
+        [SecurityFilter ("quality__allow_update")]
+        public async Task<IActionResult> Get (string status, [FromQuery] int startat, [FromQuery] int quantity) {
+            try {
+                if (quantity == 0)
+                    quantity = 50;
 
+                var (productionQualityList, total) = await _productionOrderQualityService
+                    .GetProductionOrderQualityPerStatus (status,
+                        startat, quantity);
 
-                var (productionQualityList,total) = await _productionOrderQualityService
-                                                            .GetProductionOrderQualityPerStatus(status,
-                                                            startat,quantity);
+                if (productionQualityList == null)
+                    return NotFound ();
 
-                if(productionQualityList == null)
-                    return NotFound();
-
-                return Ok(new {values = productionQualityList, total = total});
-            }
-            catch(Exception ex)
-            {
-                return StatusCode(500, ex.Message);
+                return Ok (new { values = productionQualityList, total = total });
+            } catch (Exception ex) {
+                return StatusCode (500, ex.Message);
             }
         }
 
-        [HttpGet("recipeCode/{recipeCode}")]
-        public async Task<IActionResult> GetRecipeCode(string recipeCode,[FromQuery]long startDate, [FromQuery]long endDate)
-        {
-            try{   
-
+        [HttpGet ("recipeCode/{recipeCode}")]
+        [SecurityFilter ("quality__allow_read")]
+        public async Task<IActionResult> GetRecipeCode (string recipeCode, [FromQuery] long startDate, [FromQuery] long endDate) {
+            try {
 
                 var productionQualityList = await _productionOrderQualityService
-                                                            .GetProductionOrderQaulityPerRecipeCode(recipeCode,
-                                                            startDate,endDate);
+                    .GetProductionOrderQaulityPerRecipeCode (recipeCode,
+                        startDate, endDate);
 
-                if(productionQualityList == null)
-                    return NotFound();
+                if (productionQualityList == null)
+                    return NotFound ();
 
-                return Ok(productionQualityList);
-            }
-            catch(Exception ex)
-            {
-                return StatusCode(500, ex.Message);
+                return Ok (productionQualityList);
+            } catch (Exception ex) {
+                return StatusCode (500, ex.Message);
             }
         }
 
-        [HttpGet("Date")]
-        public async Task<IActionResult> GetDate([FromQuery]long startDate, [FromQuery]long endDate)
-        {
-            try{   
-
+        [HttpGet ("Date")]
+        [SecurityFilter ("quality__allow_read")]
+        public async Task<IActionResult> GetDate ([FromQuery] long startDate, [FromQuery] long endDate) {
+            try {
 
                 var productionQualityList = await _productionOrderQualityService
-                                                            .GetProductionOrderQaulityPerDate(startDate,endDate);
+                    .GetProductionOrderQaulityPerDate (startDate, endDate);
 
-                if(productionQualityList == null)
-                    return NotFound();
+                if (productionQualityList == null)
+                    return NotFound ();
 
-                return Ok(productionQualityList);
-            }
-            catch(Exception ex)
-            {
-                return StatusCode(500, ex.Message);
+                return Ok (productionQualityList);
+            } catch (Exception ex) {
+                return StatusCode (500, ex.Message);
             }
         }
 
-        [HttpGet("id/{id}")]
-        public async Task<IActionResult> Get(int id)
-        {
-            try{
-                
+        [HttpGet ("id/{id}")]
+        [SecurityFilter ("quality__allow_read")]
+        public async Task<IActionResult> Get (int id) {
+            try {
+
                 var productionQuality = await _productionOrderQualityService
-                                                            .GetProductionOrderQualityId(id);
+                    .GetProductionOrderQualityId (id);
 
-                if(productionQuality == null)
-                    return NotFound();
+                if (productionQuality == null)
+                    return NotFound ();
 
-                return Ok(productionQuality);
-            }
-            catch(Exception ex)
-            {
-                return StatusCode(500, ex.Message);
+                return Ok (productionQuality);
+            } catch (Exception ex) {
+                return StatusCode (500, ex.Message);
             }
         }
 
-        [HttpGet("number/{number}")]
-        public async Task<IActionResult> Get(string number)
-        {
-            try{
-                
+        [HttpGet ("number/{number}")]
+        [SecurityFilter ("quality__allow_read")]
+        public async Task<IActionResult> Get (string number) {
+            try {
+
                 var productionQuality = await _productionOrderQualityService
-                                                            .GetProductionOrderQualityNumber(number);
+                    .GetProductionOrderQualityNumber (number);
 
-                if(productionQuality == null)
-                    return NotFound();
+                if (productionQuality == null)
+                    return NotFound ();
 
-                return Ok(productionQuality);
-            }
-            catch(Exception ex)
-            {
-                return StatusCode(500, ex.Message);
+                return Ok (productionQuality);
+            } catch (Exception ex) {
+                return StatusCode (500, ex.Message);
             }
         }
 
-         [HttpGet("productionOrder/{productionOrderId}")]
-        public async Task<IActionResult> GetProductionOrderId(int productionOrderId)
-        {
-            try{
-                
+        [HttpGet ("productionOrder/{productionOrderId}")]
+        [SecurityFilter ("quality__allow_read")]
+        public async Task<IActionResult> GetProductionOrderId (int productionOrderId) {
+            try {
+
                 var productionQuality = await _productionOrderQualityService
-                                                           .GetProductionOrder(productionOrderId);
+                    .GetProductionOrder (productionOrderId);
 
-                if(productionQuality == null)
-                    return NotFound();
+                if (productionQuality == null)
+                    return NotFound ();
 
-                return Ok(productionQuality);
-            }
-            catch(Exception ex)
-            {
-                return StatusCode(500, ex.Message);
+                return Ok (productionQuality);
+            } catch (Exception ex) {
+                return StatusCode (500, ex.Message);
             }
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] ProductionOrder productionOrder)
-        {
-            try{
-                if (ModelState.IsValid)
-                {
+        [SecurityFilter ("quality__allow_update")]
+        public async Task<IActionResult> Post ([FromBody] ProductionOrder productionOrder) {
+            try {
+                if (ModelState.IsValid) {
                     var productionQuality = await _productionOrderQualityService
-                                                            .AddProductionOrderQuality(productionOrder);
+                        .AddProductionOrderQuality (productionOrder);
 
-                     return Created($"api/productionOrderQuality/{productionQuality.productionOrderQualityId}", productionQuality);
-                }   
-            return BadRequest(ModelState);
-            }
-            catch(Exception ex)
-            {
-                return StatusCode(500, ex.Message);
+                    return Created ($"api/productionOrderQuality/{productionQuality.productionOrderQualityId}", productionQuality);
+                }
+                return BadRequest (ModelState);
+            } catch (Exception ex) {
+                return StatusCode (500, ex.Message);
             }
         }
 
-         [HttpPost("Waiting")]
-        public async Task<IActionResult> PostWaiting([FromBody] ProductionOrder productionOrder)
-        {
-            try{
-               
+        [HttpPost ("Waiting")]
+        [SecurityFilter ("quality__allow_update")]
+        public async Task<IActionResult> PostWaiting ([FromBody] ProductionOrder productionOrder) {
+            try {
+
                 var productionQuality = await _productionOrderQualityService
-                                                        .setProductionOrderQualityWaiting(productionOrder.productionOrderId);
-                if(productionQuality != null)
-                    return Ok(productionQuality);
-                return StatusCode(500, "");
-            }
-            catch(Exception ex)
-            {
-                return StatusCode(500, ex.Message);
+                    .setProductionOrderQualityWaiting (productionOrder.productionOrderId);
+                if (productionQuality != null)
+                    return Ok (productionQuality);
+                return StatusCode (500, "");
+            } catch (Exception ex) {
+                return StatusCode (500, ex.Message);
             }
         }
-        
     }
 }
